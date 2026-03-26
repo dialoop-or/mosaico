a2d6f81086c6090b3db680dc44a25b1c7c121acc
Oliver Riedel <riedel@dialoop.de>
2026-03-26 13:57:53 +0100

    mail config

--- server-config.js
+++ server-config.js
@@ -1,12 +1,16 @@
 // Put here your email sending configurations!
 module.exports = {
 	emailTransport: {
-		// host: 'yoursmtpserver',
-		// port: 25,
-		// auth: { user: '####', pass: '####' }
+		host: process.env.SMTP_HOST,
+		port: Number(process.env.SMTP_PORT || 587),
+		secure: process.env.SMTP_SECURE === 'true',
+		auth: {
+			user: process.env.SMTP_USER,
+			pass: process.env.SMTP_PASS
+		}
 	},
 	emailOptions: {
-		from: 'Mosaico by VOXmail <test@mosaico.io>', // sender address
+		from: process.env.SMTP_FROM || 'Mosaico by dialoop <mosaico@e-mail-serv.de>',
 		// bcc: 'mosaico@mosaico.io',
 	}
 };
